097c55fb7d60dcabcb6c79cca61c6d61072cbcf1
sayHii <sayHii@live.com>
2020-06-18 00:21:36 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public Map test(@RequestParam("str") String str) {
+
+        Map map = new HashMap<>();
+
+        map.put("code", 200);
+        map.put("msg", "str");
+        map.put("data", System.currentTimeMillis());
+
+        return map;
+    }
+
+}
